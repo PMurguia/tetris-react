8611726fe0fad9c8d3f9e19ed68ca20946244c2c
Peio Murguia <43602810+PMurguia@users.noreply.github.com>
2020-01-01 10:33:04 +0100

    player and tetromino rotation

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -4,4 +4,24 @@ export const STAGE_HEIGHT = 20;
 export const createStage = () => 
     Array.from(Array(STAGE_HEIGHT), () => 
         new Array(STAGE_WIDTH).fill([0, 'clear'])
-    )
+    );
+
+export const checkCollision = (player, stage, {x: moveX, y: moveY}) =>  {
+    for(let y = 0; y < player.tetromino.length; y++){
+        for(let x = 0; x < player.tetromino[y].length; x++) {
+
+            // Checking we're on an actual tetromino cell 
+            if(player.tetromino[y][x] !== 0) {
+                if(
+                //Check that move is inside the area's height (y)
+                !stage[y + player.pos.y + moveY] ||
+                //Checks our move i inside the game area's width (x)
+                !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+                //Checks that cell we're moving to isn't set to clear
+                stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+                ){
+                    return true;
+                }}
+        }
+    }
+};
